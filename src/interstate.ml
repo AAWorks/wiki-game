0dd3f9c699414fbf3a906ddad9ec43ffed305cfc
AAWorks <axalonso12@gmail.com>
2023-07-11 20:38:20 +0000

    interstate + bfs

--- src/interstate.ml
+++ src/interstate.ml
@@ -1,4 +1,58 @@
 open! Core
+module City = String
+
+module Interstate = struct
+  include String
+
+  let default = ""
+end
+
+module Network = struct
+  (* We can represent our social network graph as a set of connections, where
+     a connection represents a friendship between two people. *)
+  module Connection = struct
+    module T = struct
+      type t = City.t * Interstate.t * City.t [@@deriving compare, sexp]
+    end
+
+    (* This funky syntax is necessary to implement sets of [Connection.t]s.
+       This is needed to defined our [Network.t] type later. Using this
+       [Comparable.Make] functor also gives us immutable maps, which might
+       come in handy later. *)
+    include Comparable.Make (T)
+
+    let of_string s =
+      String.split s ~on:',' |> List.map ~f:(fun elem -> City.of_string elem)
+    ;;
+  end
+
+  type t = Connection.Set.t [@@deriving sexp_of]
+
+  let of_file input_file =
+    let connections =
+      In_channel.read_lines (File_path.to_string input_file)
+      |> List.concat_map ~f:(fun s ->
+           match Connection.of_string s with
+           | interstate :: second :: tail ->
+             List.mapi tail ~f:(fun i b ->
+               let road = Interstate.of_string interstate in
+               let a = List.nth_exn (second :: tail) i in
+               [ a, road, b ])
+             (* Friendships are mutual; a connection between a and b means we
+                should also consider the connection between b and a. *)
+           | _ :: [] | [] ->
+             printf
+               "ERROR: Could not parse line as connection; dropping. %s\n"
+               s;
+             [])
+      |> List.concat
+    in
+    Connection.Set.of_list connections
+  ;;
+end
+
+module G =
+  Graph.Imperative.Digraph.ConcreteBidirectionalLabeled (City) (Interstate)
 
 let load_command =
   let open Command.Let_syntax in
@@ -9,25 +63,46 @@ let load_command =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing interstates and the cities they go through"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let network = Network.of_file input_file in
+        (* This special syntax can be used to easily sexp-serialize values
+           (whose types have [sexp_of_t] implemented). *)
+        printf !"%{sexp: Network.t}\n" network]
 ;;
+
+module Dot = Graph.Graphviz.Dot (struct
+  include G
+
+  (* These functions can be changed to tweak the appearance of the generated
+     graph. Check out the ocamlgraph graphviz API
+     (https://github.com/backtracking/ocamlgraph/blob/master/src/graphviz.mli)
+     for examples of what values can be set here. *)
+  let edge_attributes _ = [ `Dir `None ] (*bish wtf*)
+  let default_edge_attributes _ = []
+  let get_subgraph _ = None
+  let vertex_attributes v = [ `Shape `Box; `Label v; `Fillcolor 1000 ]
+  let vertex_name v = v
+  let default_vertex_attributes _ = []
+  let graph_attributes _ = []
+end)
 
 let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let input_file =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing all interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing all interstates and the cities they go \
+             through"
       and output_file =
         flag
           "output"
@@ -35,8 +110,15 @@ let visualize_command =
           ~doc:"FILE where to write generated graph"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        ignore (output_file : File_path.t);
+        let network = Network.of_file input_file in
+        let graph = G.create () in
+        Set.iter network ~f:(fun group ->
+          (* [G.add_edge] auomatically adds the endpoints as vertices in the
+             graph if they don't already exist. *)
+          G.add_edge_e graph group);
+        Dot.output_graph
+          (Out_channel.create (File_path.to_string output_file))
+          graph;
         printf !"Done! Wrote dot file to %{File_path}\n%!" output_file]
 ;;
 
